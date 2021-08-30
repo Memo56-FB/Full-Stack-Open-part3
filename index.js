32872587d098b3838ba705176e137bbed3fe6fb1
Guillermo Farfan <memo56_fb@hotmail.com>
2021-08-30 14:30:11 -0500

    exercise 3.18

--- index.js
+++ index.js
@@ -49,6 +49,16 @@ app.delete('/api/persons/:id',(request,response,next)=>{
         .then(result => response.status(204).end())
         .catch(error => next(error))
 })
+app.put('/api/persons/:id',(req,res,next)=>{
+    const body = req.body
+    const person = {
+        name: body.name,
+        number:body.number
+    }
+    Person.findByIdAndUpdate(req.params.id, person, {new:true})
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error))
+})
 
 app.get('/info',(request,response)=>{
     response.send(`
