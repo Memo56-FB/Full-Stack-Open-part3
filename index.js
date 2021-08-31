4f3f9c68794f37820afec8ac48fc7c509d3a3392
Guillermo Farfan <memo56_fb@hotmail.com>
2021-08-31 09:17:58 -0500

    exercise 3.22

--- index.js
+++ index.js
@@ -1,81 +1,78 @@
-const express = require('express')
-const morgan = require('morgan')
-const Person = require('./modules/person')
-const errorHandler = require('./errorHandler')
+const express = require("express")
+const morgan = require("morgan")
+const Person = require("./modules/person")
+const errorHandler = require("./errorHandler")
 
-require('dotenv').config()
+require("dotenv").config()
 const app = express()
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.use(express.json())
-app.use(express.static('build'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content-person'))
+app.use(express.static("build"))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content-person"))
 
-app.get('/api/persons',(request,response)=>{
-    Person.find({}).then(persons => response.json(persons) )
-    
+app.get("/api/persons",(_request,response) => {
+	Person.find({}).then(persons => response.json(persons) )
 })
 
-app.get('/api/persons/:id',(request,response,next)=>{
-    const id = request.params.id
-    Person.findById(id)
-        .then(person => {
-            person ? response.json(person) : response.status(404).end()    
-        })
-        .catch(error => {
-            // console.log(error)
-            next(error)
-        })
-    
+app.get("/api/persons/:id",(request,response,next) => {
+	const id = request.params.id
+	Person.findById(id)
+		.then(person => {
+			person ? response.json(person) : response.status(404).end()
+		})
+		.catch(error => {
+			next(error)
+		})
 })
 
-app.post('/api/persons',(request,response,next)=>{
-    const body = request.body
+app.post("/api/persons",(request,response,next) => {
+	const body = request.body
 
-    const person = new Person({
-        name:body.name,
-        number:body.number
-    })
-    person.save()
-        .then(savedPerson => response.json(savedPerson.toJSON()))
-        .catch(error => next(error))
-        
+	const person = new Person({
+		name:body.name,
+		number:body.number
+	})
+	person.save()
+		.then(savedPerson => response.json(savedPerson.toJSON()))
+		.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request,response,next)=>{
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => response.status(204).end())
-        .catch(error => next(error))
+app.delete("/api/persons/:id",(request,response,next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(() => response.status(204).end())
+		.catch(error => next(error))
 })
-app.put('/api/persons/:id',(req,res,next)=>{
-    const body = req.body
-    const person = {
-        name: body.name,
-        number:body.number
-    }
-    Person.findByIdAndUpdate(req.params.id, person, {new:true})
-        .then(updatedPerson => res.json(updatedPerson))
-        .catch(error => next(error))
+app.put("/api/persons/:id",(req,res,next) => {
+	const body = req.body
+	const person = {
+		name: body.name,
+		number:body.number
+	}
+	Person.findByIdAndUpdate(req.params.id, person, { new:true })
+		.then(updatedPerson => res.json(updatedPerson))
+		.catch(error => next(error))
 })
 
-app.get('/info',(request,response)=>{
-    Person.find({}).then(persons => {
-        response.send(`
+app.get("/info",(_request,response) => {
+	Person.find({}).then(persons => {
+		response.send(`
             <p>Phonebook has info for ${persons.length} people</p>
             <p> ${new Date}</p>
         `)
-    })
+	})
 })
 
-morgan.token('content-person',(req,res)=>{
-    return `{"name":"${req.body['name']}","number:"${req.body['number']}"}`
+morgan.token("content-person",(req) => {
+	return `{"name":"${req.body["name"]}","number:"${req.body["number"]}"}`
 })
-const unknownEndpoint = (req,res) =>{
-    res.status(404).send({error:"unknown endpoint"})
+const unknownEndpoint = (_req,res) => {
+	res.status(404).send({ error:"unknown endpoint" })
 }
 
-  app.use(unknownEndpoint)
-  app.use(errorHandler)
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
